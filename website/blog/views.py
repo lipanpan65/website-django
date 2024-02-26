f1c9268fe2b9430e6ed1ce019802b8962e5260da
李盼盼 <lipanpan@192.168.1.3>
2024-02-26 22:13:03 +0800

    feat: 文章的编辑

--- website/blog/views.py
+++ website/blog/views.py
@@ -3,14 +3,17 @@ from rest_framework import viewsets
 
 from blog import models
 from blog import serializers
+from components.pagination import TablePageNumberPagination
 
 
 # from website.blog import
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
-    queryset = models.Article.objects.all()
+    queryset = models.Article.objects.all().order_by('-create_time')
     serializer_class = serializers.ArticleSerializer
+    pagination_class = TablePageNumberPagination
+
 
     # def update(self, request, *args, **kwargs):
     #     print(request)
