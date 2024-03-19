0b373dc130fa3de0425099fda9b7a30af5dde207
lipanpan65 <61178683+lipanpan65@users.noreply.github.com>
2024-03-19 14:57:19 +0800

    Update response.py
    update BaseResult

--- website/components/response.py
+++ website/components/response.py
@@ -1,5 +1,28 @@
 
+# https://juejin.cn/post/7307502467826008091
+from abc import ABC
+from collections.abc import MutableMapping
 
 
-class Result(object):
-    pass
+class BaseResult(dict):
+
+    def __init__(self, message='成功', data=None, *args, **kwargs):
+        super(BaseResult, self).__init__()
+        if kwargs:
+            data = self.get('data') or dict()
+            data.update(kwargs)
+        self.update(success=True, message=message, data=data)
+
+    def success(self, message='操作成功', data=None):
+        self.update(message=message, data=data)
+
+    def failure(self, message='操作失败', data=None):
+        self.update(success=False, message=message, data=data)
+
+    @property
+    def ok(self):
+        return self.get('success')
+
+    @property
+    def data(self):
+        return self.get('data')
