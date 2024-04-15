0d1c50b2ede70055686b19f28cbf06ed54df6dca
李盼盼 <lipanpan@192.168.1.2>
2024-04-16 01:12:42 +0800

    feat: create munus model

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,10 +1,13 @@
 import logging
 from rest_framework import viewsets
 from rest_framework import status
+from rest_framework.decorators import action
 from blog import models
 from blog import serializers
 from components.pagination import TablePageNumberPagination, SizeTablePageNumberPagination
 from components.response import ResultEnum, ApiResult
+
+logger = logging.getLogger()
 
 
 # from components.exceptions import BizException
@@ -25,11 +28,13 @@ class ArticleViewSet(viewsets.ModelViewSet):
     #     raise ValueError('this is test')
     #     return super(ArticleViewSet, self).list(request)
 
+    @action(methods=['POST'], detail=False)
     def publish(self, request, *args, **kwargs):
         """
         发布文章
         """
-        pass
+        instance = self.get_object()
+
         return ApiResult.success()
 
 
@@ -37,3 +42,19 @@ class ArticleCategoryViewSet(viewsets.ModelViewSet):
     queryset = models.ArticleCategory.objects.all().order_by('-create_time')
     serializer_class = serializers.ArticleCategorySerializer
     pagination_class = SizeTablePageNumberPagination
+
+    @action(methods=['GET'], detail=False)
+    def validate_category_name(self, request, *args, **kwargs):
+        """
+        校验名称是否存在
+        """
+        # logger.info("[校验分类名称是否存在] %s" % request)
+        logger.info("[校验分类名称是否存在] %s" % dir(request))
+        logger.info("[校验分类名称是否存在] %s" % request.query_params)
+        category_name = request.query_params.get('category_name')
+        # TODO 校验字符串是否为空
+        queryset = self.queryset.filter(category_name=category_name)
+        if queryset.exists():
+            message = '分类名称：%s 已经存在' % category_name
+            return ApiResult.failure(message=message, data=queryset.values())
+        return ApiResult.success()
